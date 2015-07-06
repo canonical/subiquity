dabdca23208315127624dc86e70ae7e5342ea885
Ryan Harper <ryan.harper@canonical.com>
2015-07-06 14:00:50 -0500

    Detect successful install
    Detect successful install by looking at output from
    curtin logged to a file.  Also read for input from
    user on successful install.
    
    Signed-off-by: Ryan Harper <ryan.harper@canonical.com>

--- bin/curtin_wrap.sh
+++ bin/curtin_wrap.sh
@@ -3,6 +3,7 @@
 CMD="-v --showtrace install -c /tmp/subiquity-config.yaml cp:///"
 CURTIN="/usr/local/curtin/bin/curtin"
 OUTPUT="/tmp/.curtin_wrap_ran"
+INSTALL_OK="Installation finished. No error reported."
 
 if [ -e $CURTIN ]; then 
    $CURTIN $CMD | tee -a $OUTPUT
@@ -12,9 +13,12 @@ else
    echo "$CURTIN not found" > $OUTPUT
    echo $CURTIN $CMD | tee -a $OUTPUT
 fi
-if [ $RV == 0 ]; then
+
+if tail -n 1 $OUTPUT | grep -q "$INSTALL_OK"; then
+    echo "Curtin installed OK, shutting down (Press Any Key)"
+    read a
     echo "Shutting down after running $CURTIN $CMD"
-    #telinit 0
+    telinit 0
 else
     echo "Error running curting, exiting..."
     exit 1;
