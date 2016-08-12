a23840411dff89e6cf7f6bda40d09507c613a281
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2016-08-12 14:58:21 -0400

    Iterate all IPv4 addresses on an interface.

--- console_conf/ui/views/login.py
+++ console_conf/ui/views/login.py
@@ -73,13 +73,14 @@ class LoginView(BaseView):
         login_text = local_tpl.format(**login_info)
         login_text += remote_tpl.format(**login_info)
         for iface in self.ifaces:
-            try:
-                ip = str(iface.ip4[0]).split("/")[0]
-            except IndexError:
-                ip = None
-            if ip is not None:
-                ssh_iface = "    ssh %s@%s" % (user.username, ip)
-                ssh += [Padding.center_50(Text(ssh_iface))]
+            for ip4 in iface.ip4:
+                try:
+                    ip = str(ip4).split("/")[0]
+                except IndexError:
+                    ip = None
+                if ip is not None:
+                    ssh_iface = "    ssh %s@%s" % (user.username, ip)
+                    ssh += [Padding.center_50(Text(ssh_iface))]
 
         sl += [Text(login_text),
                Padding.line_break("")] + ssh
