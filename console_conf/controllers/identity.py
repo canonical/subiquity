7d9d8b019961c20c5a9a0957ec6ceb779aafb686
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2016-08-23 16:19:26 -0400

    Release 0.0.9:
    * Release 0.0.9:
      - Probe network before showing Login screen.

--- console_conf/controllers/identity.py
+++ console_conf/controllers/identity.py
@@ -37,6 +37,7 @@ class IdentityController(BaseIdentityController):
         self.ui.set_footer(footer)
 
         net_model = self.controllers['Network'].model
+        net_model.probe_network()
         configured_ifaces = net_model.get_configured_interfaces()
         login_view = LoginView(self.opts,
                                self.model,
