7c7fc8218f73404bce89f824eebef2365de29baf
Michael Hudson-Doyle <michael.hudson@canonical.com>
2016-11-10 11:59:39 +1300

    shuffle things around so that it works for a user created via an assertion

--- console_conf/controllers/identity.py
+++ console_conf/controllers/identity.py
@@ -15,7 +15,9 @@
 
 import json
 import logging
+import os
 import subprocess
+import sys
 
 from subiquitycore.controllers.identity import BaseIdentityController
 from subiquitycore.utils import disable_first_boot_service, run_command
@@ -34,6 +36,64 @@ Public SSH keys were added to the device for remote access.
 """
 
 
+def get_device_owner():
+    """ Check if device is owned """
+
+    # TODO: use proper snap APIs.
+    try:
+        extrausers_fp = open('/var/lib/extrausers/passwd', 'r')
+    except FileNotFoundError:
+        return None
+    with extrausers_fp:
+        passwd_line = extrausers_fp.readline()
+        if passwd_line and len(passwd_line) > 0:
+            passwd = passwd_line.split(':')
+            result = {
+                'realname': passwd[4].split(',')[0],
+                'username': passwd[0],
+                'homedir': passwd[5],
+                }
+            return result
+    return None
+
+
+def write_login_details(fp, realname, username, ips, fingerprints):
+    fp.write(login_details_tmpl.format(realname=realname, username=username))
+    for ip in ips:
+        fp.write("    ssh %s@%s\n"%(username, ip))
+    fp.write("\nSSH keys with the following fingerprints can be used to log in:\n\n")
+    for fingerprint in fingerprints:
+        fp.write("    " + fingerprint + "\n")
+    fp.write("\nPressing enter after setting a password will allow you to log in here.\n")
+
+
+def write_login_details_standalone():
+    owner = get_device_owner()
+    if owner is None:
+        # Nothing much we can do :/
+        print("No device owner details found")
+        return 0
+    from probert import network
+    from subiquitycore.models.network import NETDEV_IGNORED_IFACE_NAMES, NETDEV_IGNORED_IFACE_TYPES
+    import ipaddress
+    import operator
+    import socket
+    observer = network.UdevObserver()
+    observer.start()
+    ips = []
+    for l in sorted(observer.links.values(), key=operator.attrgetter('name')):
+        if l.type in NETDEV_IGNORED_IFACE_TYPES:
+            continue
+        if l.name in NETDEV_IGNORED_IFACE_NAMES:
+            continue
+        ips.extend([str(ipaddress.IPv4Interface(a).ip) for a in l.ip.get(socket.AF_INET, [])])
+        ips.extend([str(ipaddress.IPv6Interface(a).ip) for a in l.ip.get(socket.AF_INET6, [])])
+    key_file = os.path.join(owner['homedir'], ".ssh/authorized_keys")
+    fingerprints = run_command(['ssh-keygen', '-lf', key_file])['output'].replace('\r', '').splitlines()
+    write_login_details(sys.stdout, owner['realname'], owner['username'], ips, fingerprints)
+    return 0
+
+
 class IdentityController(BaseIdentityController):
     identity_view = IdentityView
 
@@ -44,29 +104,10 @@ class IdentityController(BaseIdentityController):
         self.ui.set_header(title, excerpt)
         self.ui.set_footer(footer, 40)
         self.ui.set_body(self.identity_view(self.model, self, self.opts, self.loop))
-        device_owner = self.get_device_owner()
+        device_owner = get_device_owner()
         if device_owner is not None:
             self.model.add_user(device_owner)
             self.login()
-
-    def get_device_owner(self):
-        """ Check if device is owned """
-
-        # TODO: use proper snap APIs.
-        try:
-            extrausers_fp = open('/var/lib/extrausers/passwd', 'r')
-        except FileNotFoundError:
-            return None
-        with extrausers_fp:
-            passwd_line = extrausers_fp.readline()
-            if passwd_line and len(passwd_line) > 0:
-                passwd = passwd_line.split(':')
-                result = {
-                    'realname': passwd[4].split(',')[0],
-                    'username': passwd[0],
-                    }
-                return result
-        return None
 
     def identity_done(self, email):
         if self.opts.dry_run:
@@ -92,25 +133,22 @@ class IdentityController(BaseIdentityController):
                     'username': data['username'],
                     }
                 ssh_keys = data['ssh-keys']
-                login_details_path = '/var/lib/console-conf/login-details.txt'
+                os.makedirs('/run/console-conf', exist_ok=True)
+                login_details_path = '/run/console-conf/login-details.txt'
                 self.model.add_user(result)
         log.debug('ssh_keys %s', ssh_keys)
         fingerprints = []
         for key in ssh_keys:
             keygen_result = subprocess.Popen(['ssh-keygen', '-lf', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             fingerprint, err = keygen_result.communicate(key.encode('utf-8'))
-            fingerprints.append(fingerprint.decode('utf-8', 'replace').replace('\r', ''))
+            fingerprints.append(fingerprint.decode('utf-8', 'replace').replace('\r', '').strip())
         log.debug('fingerprints %s', fingerprints)
+        ips = []
         net_model = self.controllers['Network'].model
+        for dev in net_model.get_all_netdevs():
+            ips.extend(dev.actual_ip_addresses)
         with open(login_details_path, 'w') as fp:
-            fp.write(login_details_tmpl.format(**result))
-            for dev in net_model.get_all_netdevs():
-                for ip in dev.actual_ip_addresses:
-                    fp.write("    ssh %s@%s\n"%(result['username'], ip))
-            fp.write("\nSSH keys with the following fingerprints can be used to log in:\n\n")
-            for fingerprint in fingerprints:
-                fp.write("    " + fingerprint)
-            fp.write("\nPressing enter after setting a password will allow you to log in here.\n")
+            write_login_details(fp, result['realname'], result['username'], ips, fingerprints)
         self.login()
 
     def cancel(self):
