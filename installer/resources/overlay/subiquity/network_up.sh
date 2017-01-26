fe39d3ec9dcff54312da3a9a84135440efb5e289
Michael Hudson-Doyle <michael.hudson@canonical.com>
2017-01-27 09:58:25 +1300

    remove lots of garbage from installer cloud image config

--- installer/resources/overlay/subiquity/network_up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-SKIP_IFACE="(lo|lxcbr0)"
-rm -f /etc/network/interfaces
-echo -e "auto lo\niface lo inet loopback\n" > /etc/network/interfaces 
-/bin/ls -1 /sys/class/net | egrep -v "^${SKIP_IFACE}$" | xargs -i bash -c 'D={}; echo -e "auto $D\niface $D inet dhcp"' | tee /etc/network/interfaces
-/bin/ls -1 /sys/class/net | egrep -v "^${SKIP_IFACE}$" | xargs -i ifup {}
