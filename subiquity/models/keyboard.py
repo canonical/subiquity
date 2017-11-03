e91eaeff3796360f7d02a8e710e08ec65844ecef
Michael Hudson-Doyle <michael.hudson@canonical.com>
2017-11-03 14:48:01 +1300

    very start of keyboard view

--- subiquity/models/keyboard.py
+++ subiquity/models/keyboard.py
@@ -1,5 +1,6 @@
 from lxml import etree
 
+
 class Keyboard:
     def __init__(self, code, desc):
         self.code = code
@@ -10,19 +11,26 @@ class Keyboard:
         return "Keyboard({}, {}) {} {}".format(self.code, self.desc, self.variants, self.languages)
 
 
+class KeyboardModel:
+    def __init__(self):
+        self.keyboards = [] # Keyboard objects
+
+    def parse(self, fname):
+        t = etree.parse(fname)
+        for layout in t.xpath("//layoutList/layout"):
+            c = layout.find("configItem")
+            code = c.find("name").text
+            description = c.find("description").text
+            keyboard = Keyboard(code, description)
+            for lang in c.xpath("languageList/iso639Id/text()"):
+                keyboard.languages.add(lang)
+            for v in layout.xpath("variantList/variant/configItem"):
+                keyboard.variants[v.find("name").text] = v.find("description").text
+            self.keyboards.append(keyboard)
+
+
 def main():
     keyboards = {}
-    t = etree.parse("/usr/share/X11/xkb/rules/base.xml")
-    for layout in t.xpath("//layoutList/layout"):
-        c = layout.find("configItem")
-        code = c.find("name").text
-        description = c.find("description").text
-        keyboard = keyboards[code] = Keyboard(code, description)
-        for lang in c.xpath("languageList/iso639Id/text()"):
-            keyboard.languages.add(lang)
-        for v in layout.xpath("variantList/variant/configItem"):
-            keyboard.variants[v.find("name").text] = v.find("description").text
-    print(keyboards["us"])
 
 
 if __name__ == "__main__":
---
+++ subiquity/ui/views/keyboard.py
@@ -0,0 +1,79 @@
+# Copyright 2015 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import re
+
+from urwid import (
+    connect_signal,
+    )
+
+from subiquitycore.ui.container import (
+    ListBox,
+    )
+from subiquitycore.ui.form import (
+    Form,
+    FormField,
+    )
+from subiquitycore.ui.selector import Option, Selector
+from subiquitycore.ui.utils import button_pile, Padding
+from subiquitycore.view import BaseView
+
+log = logging.getLogger("subiquity.ui.views.keyboard")
+
+
+class ChoiceField(FormField):
+
+    def __init__(self, caption=None, help=None, choices=[]):
+        self.choices = choices
+
+    def _make_widget(self, form):
+        return Selector(self.choices)
+
+class KeyboardForm(Form):
+
+    layout = ChoiceField()
+    variant = ChoiceField()
+
+
+class KeyboardView(BaseView):
+    def __init__(self, model, controller, opts):
+        self.model = model
+        self.controller = controller
+        self.opts = opts
+
+        self.form = KeyboardForm()
+        opts = []
+        us_keyboard = None
+        for keyboard in model.keyboards:
+            if keyboard.code == "us":
+                us_keyboard = keyboard
+            opts.append(Option(keyboard.desc, True, keyboard))
+        opts.sort(key=lambda o:o.label)
+        self.form.layout.widget._options = opts
+        self.form.layout.widget.value = us_keyboard
+        connect_signal(self.form, 'submit', self.done)
+
+        body = [
+            Padding.center_90(self.form.as_rows()),
+            Padding.line_break(""),
+            button_pile([self.form.buttons]),
+        ]
+        super().__init__(ListBox(body))
+
+    def done(self, result):
+        pass
+
+
