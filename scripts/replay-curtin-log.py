6b156766bb83f9edb08a99a43edffb6aa6a8886e
Michael Hudson-Doyle <michael.hudson@canonical.com>
2018-02-28 15:57:35 +1300

    change replay-curtin-log to give a more realistic experience in dry-run mode

--- scripts/replay-curtin-log.py
+++ scripts/replay-curtin-log.py
@@ -1,54 +1,33 @@
 #!/usr/bin/python3
 
 import json
-import logging
 import os
 import sys
 import time
 
-from curtin import reporter
-from curtin.reporter import events
-
-logger = logging.getLogger('')
-logger.setLevel('DEBUG')
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter("%(asctime)s %(name)s:%(lineno)d %(message)s"))
-logger.addHandler(handler)
+from systemd import journal
 
 json_file = sys.argv[1]
-
-c = {'subiquity': {'type': 'journald', 'identifier': sys.argv[2]}, 'print': {'type': 'print'}}
-
-reporter.update_configuration(c)
-
-## ev_dict = {
-##     "origin": "curtin",
-##     "event_type": "start",
-##     "level": "DEBUG",
-##     "timestamp": 1505187478.3402257,
-##     "name": "cmd-install",
-##     "description": "curtin command install"
-##     }
-
-class FakeEvent:
-    def __init__(self, ev_dict):
-        self._ev_dict = ev_dict
-        for k, v in ev_dict.items():
-            setattr(self, k, v)
-    def as_string(self):
-        return '{0}: {1}: {2}'.format(
-            self._ev_dict['event_type'], self._ev_dict['name'], self._ev_dict['description'])
-    def as_dict(self):
-        return self._ev_dict
+event_identifier = sys.argv[2]
 
 scale_factor = float(os.environ.get('SUBIQUITY_REPLAY_TIMESCALE', "4"))
 
+def time_for_entry(e):
+    return int(e['__MONOTONIC_TIMESTAMP'])/1e6
+
+def report(e):
+    if e['SYSLOG_IDENTIFIER'].startswith("curtin_event"):
+        e['SYSLOG_IDENTIFIER'] = event_identifier
+        e['CODE_LINE'] = int(e['CODE_LINE'])
+        journal.send(**e)
+    elif e['SYSLOG_IDENTIFIER'].startswith("curtin_log"):
+        print(e['MESSAGE'], flush=True)
+
 prev_ev = None
 for line in open(json_file):
-    d = json.loads(line.strip())
-    ev = FakeEvent(d)
+    ev = json.loads(line.strip())
     if prev_ev is not None:
-        events.report_event(prev_ev)
-        time.sleep(min((ev.timestamp - prev_ev.timestamp), 8)/scale_factor)
+        report(prev_ev)
+        time.sleep(min((time_for_entry(ev) - time_for_entry(prev_ev)), 8)/scale_factor)
     prev_ev = ev
-events.report_event(prev_ev)
+report(prev_ev)
