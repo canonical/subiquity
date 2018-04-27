84df76afe9da960902413d227da3d2e2c24494e5
Michael Hudson-Doyle <michael.hudson@canonical.com>
2018-04-27 13:40:05 +1200

    try to fix travis

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -5,6 +5,7 @@ apt-get -y dist-upgrade
 apt-get install -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel curtin
 pip3 install -r requirements.txt
 python3 setup.py build
+python3 setup.py build_ext -i
 
 python3 -m unittest discover
 # The --foreground is important to avoid subiquity getting SIGTTOU-ed.
