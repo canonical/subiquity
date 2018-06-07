270ba0992a8dc83ec6493a944e7c6dba9cc6a94e
Michael Hudson-Doyle <michael.hudson@canonical.com>
2018-06-07 12:50:22 +1200

    fix selectability in non-scrolling stretchy

--- subiquitycore/ui/stretchy.py
+++ subiquitycore/ui/stretchy.py
@@ -136,7 +136,7 @@ class StretchyOverlay(urwid.Widget):
 
     def keypress(self, size, key):
         top_size, scrollbar_visible = self._top_size(size, True)
-        self.listbox._selectable = (
+        self.listbox.base_widget._selectable = (
             scrollbar_visible or self.stretchy.stretchy_w.selectable())
         return self.top_w.keypress(top_size, key)
 
