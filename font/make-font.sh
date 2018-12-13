e8bd1b8eb7297258ee0af0d209285504d246d140
Michael Hudson-Doyle <michael.hudson@canonical.com>
2018-12-13 13:56:01 +1300

    fix font equivalents

--- font/make-font.sh
+++ font/make-font.sh
@@ -11,7 +11,7 @@ cd console-setup-*/Fonts
 ./bdf2psf \
     --log ${SUBIQUITY_FONT_DIR}/subiquity.log \
     ${SUBIQUITY_FONT_DIR}/subiquity.bdf+./bdf/georgian16.bdf+./bdf/unifont.bdf+./bdf/h16.bdf+./bdf/etl16-unicode.bdf \
-    ${SUBIQUITY_FONT_DIR}/subiquity.equivalents+./standard.equivalents \
+    ./standard.equivalents+${SUBIQUITY_FONT_DIR}/subiquity.equivalents \
     ${SUBIQUITY_FONT_DIR}/subiquity.set+./ascii.set+./linux.set+./fontsets/Uni2.512+:./useful.set \
     512 \
     ${SUBIQUITY_FONT_DIR}/subiquity.psf
