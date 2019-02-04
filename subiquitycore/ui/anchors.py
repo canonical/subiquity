0b3d44f9118a152d75183c52444d790e8d8206c5
Michael Hudson-Doyle <michael.hudson@canonical.com>
2019-02-04 16:17:58 +1300

    oops

--- subiquitycore/ui/anchors.py
+++ subiquitycore/ui/anchors.py
@@ -58,8 +58,8 @@ class Footer(WidgetWrap):
 
     def __init__(self, message, current, complete):
         if isinstance(message, str):
-            message_widget = Text(message)
-        message_widget = Padding.center_79(message_widget, min_width=76)
+            message = Text(message)
+        message = Padding.center_79(message, min_width=76)
         progress_bar = Padding.center_60(
             StepsProgressBar(normal='progress_incomplete',
                              complete='progress_complete',
@@ -67,6 +67,6 @@ class Footer(WidgetWrap):
         status = [
             progress_bar,
             Padding.line_break(""),
-            message_widget,
+            message,
         ]
         super().__init__(Color.frame_footer(Pile(status)))
