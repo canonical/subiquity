d381ff752cda9352723252190ab1ed4927f00a39
Michael Hudson-Doyle <michael.hudson@canonical.com>
2019-05-21 11:04:37 +1200

    display information about partitions in delete confirmation
    For a while subiquity only allowed you to delete RAIDs/VGs with no
    partitions. Now that we do, include information about the doomed
    partitions in the confirmation dialog.

--- subiquity/ui/views/filesystem/delete.py
+++ subiquity/ui/views/filesystem/delete.py
@@ -17,11 +17,27 @@ import logging
 from urwid import Text
 
 from subiquitycore.ui.buttons import danger_btn, other_btn
+from subiquitycore.ui.table import ColSpec, TablePile, TableRow
 from subiquitycore.ui.utils import button_pile
 from subiquitycore.ui.stretchy import Stretchy
 
+from subiquity.models.filesystem import humanize_size
+
 
 log = logging.getLogger('subiquity.ui.filesystem.disk_info')
+
+
+def summarize_partitions(obj):
+    # [ label, size, annotations, usage comment ]
+    rows = []
+    for p in obj.partitions():
+        row = [
+            Text(p.short_label),
+            Text(humanize_size(p.size), align='right'),
+            Text(", ".join(p.annotations + p.usage_labels()))
+            ]
+        rows.append(TableRow(row))
+    return TablePile(rows, colspecs={2: ColSpec(can_shrink=True)})
 
 
 class ConfirmDeleteStretchy(Stretchy):
@@ -33,34 +49,53 @@ class ConfirmDeleteStretchy(Stretchy):
         title = _("Confirm deletion of {}").format(obj.desc())
 
         lines = [
-            _("Do you really want to delete {}?").format(obj.label),
-            "",
+            Text(_("Do you really want to delete the {desc} {label}?").format(
+                desc=obj.desc(), label=obj.label)),
+            Text(""),
         ]
+        stretchy_index = 0
         fs = obj.fs()
         if fs is not None:
             m = fs.mount()
             if m is not None:
-                lines.append(_(
+                lines.append(Text(_(
                     "It is formatted as {fstype} and mounted at "
                     "{path}").format(
                         fstype=fs.fstype,
-                        path=m.path))
+                        path=m.path)))
             else:
-                lines.append(_(
+                lines.append(Text(_(
                     "It is formatted as {fstype} and not mounted.").format(
-                        fstype=fs.fstype))
+                        fstype=fs.fstype)))
+        elif len(obj.partitions()) > 0:
+            n = len(obj.partitions())
+            if obj.type == "lvm_volgroup":
+                if n == 1:
+                    things = _("logical volume")
+                else:
+                    things = _("logical volumes")
+            else:
+                if n == 1:
+                    things = _("partition")
+                else:
+                    things = _("partitions")
+            lines.append(Text(_("It contains {n} {things}:").format(
+                n=n, things=things)))
+            lines.append(Text(""))
+            stretchy_index = len(lines)
+            lines.append(summarize_partitions(obj))
         else:
-            lines.append(_("It is not formatted or mounted."))
+            lines.append(Text(_("It is not formatted or mounted.")))
+
         delete_btn = danger_btn(label=_("Delete"), on_press=self.confirm)
-        widgets = [
-            Text("\n".join(lines)),
+        widgets = lines + [
             Text(""),
             button_pile([
                 delete_btn,
                 other_btn(label=_("Cancel"), on_press=self.cancel),
                 ]),
         ]
-        super().__init__(title, widgets, 0, 2)
+        super().__init__(title, widgets, stretchy_index, len(lines)+1)
 
     def confirm(self, sender=None):
         self.parent.controller.delete(self.obj)
