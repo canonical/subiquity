ae033101e616c9f3dea09fbb9eaac5c0d540bb23
Michael Hudson-Doyle <michael.hudson@canonical.com>
2019-11-20 23:05:08 +1300

    code for uploading to daisy

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -4,6 +4,6 @@ set -eux
 cat /etc/apt/sources.list | sed -n 's/-updates/-proposed/p' > /etc/apt/sources.list.d/proposed.list
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
-apt-get install -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel curtin pep8 python3-pyflakes
+apt-get install -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel curtin pep8 python3-pyflakes python3-bson
 pip3 install -r requirements.txt
 python3 setup.py build
