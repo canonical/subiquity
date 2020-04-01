74d8280752dbcc68ee3fd698296a4c05d21f5fa1
Michael Hudson-Doyle <michael.hudson@canonical.com>
2020-04-02 12:06:51 +1300

    unmount /target just before starting install
    rather than at process startup.
    
    otherwise doing something like ssh-ing to installer@ while an install is
    running will unmount /target, with hilarious results.

--- subiquity/controllers/filesystem.py
+++ subiquity/controllers/filesystem.py
@@ -18,8 +18,6 @@ import json
 import logging
 import os
 import select
-import shutil
-import sys
 
 import pyudev
 
@@ -29,7 +27,6 @@ from subiquitycore.async_helpers import (
     SingleInstanceTask,
     )
 from subiquitycore.utils import (
-    arun_command,
     run_command,
     )
 
@@ -173,17 +170,6 @@ class FilesystemController(SubiquityController):
         self._start_task = schedule_task(self._start())
 
     async def _start(self):
-        target = self.app.base_model.target
-        if os.path.exists(target):
-            cmd = [
-                sys.executable, '-m', 'curtin', 'unmount',
-                '-t', self.app.base_model.target,
-                ]
-            if self.opts.dry_run:
-                cmd = ['sleep', "0.2"]
-            await arun_command(cmd)
-            if not self.opts.dry_run:
-                shutil.rmtree(self.app.base_model.target)
         context = pyudev.Context()
         self._monitor = pyudev.Monitor.from_netlink(context)
         self._monitor.filter_by(subsystem='block')
