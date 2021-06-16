6ab29c5868b814c95517fd32ce7a3d0f798dff57
Michael Hudson-Doyle <michael.hudson@canonical.com>
2021-06-17 10:37:35 +1200

    install packages with -o APT::Get::Always-Include-Phased-Updates=true
    to work around bug #1925745 in apt until it is SRUed.

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -o APT::Get::Always-Include-Phased-Updates=true -y dist-upgrade
 mkdir -p /etc/systemd/system/zfs-mount.service.d/
 cat >/etc/systemd/system/zfs-mount.service.d/override.conf <<EOF
 [Unit]
@@ -11,6 +11,6 @@ ConditionPathExists=/dev/zfs
 EOF
 cp -r /etc/systemd/system/zfs-mount.service.d/ /etc/systemd/system/zfs-share.service.d/
 systemctl daemon-reload
-apt-get install -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel curtin pep8 python3-pyflakes python3-bson make
+apt-get install -o APT::Get::Always-Include-Phased-Updates=true -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel curtin pep8 python3-pyflakes python3-bson make
 pip3 install -r requirements.txt
 python3 setup.py build
