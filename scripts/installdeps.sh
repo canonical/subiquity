f9410e4e865fd2088046d46ac24f36ed202fbd92
Michael Hudson-Doyle <michael.hudson@canonical.com>
2021-07-02 13:49:40 +1200

    install probert and curtin via Makefile for testing
    rather than pip or apt respectively

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -11,6 +11,9 @@ ConditionPathExists=/dev/zfs
 EOF
 cp -r /etc/systemd/system/zfs-mount.service.d/ /etc/systemd/system/zfs-share.service.d/
 systemctl daemon-reload
-apt-get install -o APT::Get::Always-Include-Phased-Updates=true -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel curtin pep8 python3-pyflakes python3-bson make
+apt-get install -o APT::Get::Always-Include-Phased-Updates=true -y --no-install-recommends libnl-3-dev libnl-genl-3-dev libnl-route-3-dev libsystemd-dev python3-distutils-extra pkg-config python3.5 python3-pip git lsb-release python3-setuptools gcc python3-dev python3-wheel pep8 python3-pyflakes python3-bson make
 pip3 install -r requirements.txt
-python3 setup.py build
+
+make gitdeps
+
+PYTHONPATH=.:./curtin:./probert python3 setup.py build
