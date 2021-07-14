9f1478cd966b136c1819daa1808a92fa8888ac00
Dan Bungert <daniel.bungert@canonical.com>
2021-07-13 18:13:46 -0600

    Add new schema comparison, robust to timezone changes (#1002)
    * Add new schema comparison, robust to timezone changes
    
    Improve the schema comparison - mostly I'm worried about the
    non-timezone items, spot check the timezone list for a few that should
    be there.
    
    * Incorporate feedback - pop

---
+++ scripts/schema-cmp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# usage: schema-cmp.py expected.json actual.json
+# schema comparison should match, except timezones which we just spot
+# check for a few expected values and expect that reality is a superset
+import json
+import sys
+
+def load(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    tz = data['properties']['timezone'].pop('enum')
+    return data, tz
+
+expected, _ = load(sys.argv[1])
+actual, actual_tz = load(sys.argv[2])
+
+if expected != actual:
+    print('schema mismatch')
+    print('expected:')
+    print(expected)
+    print('actual:')
+    print(actual)
+    sys.exit(1)
+
+expected_tz = [
+    '',
+    'geoip',
+    'UTC',
+    'America/New_York',
+]
+
+for tz in expected_tz:
+    assert tz in actual_tz, f'tz {tz} not found'
