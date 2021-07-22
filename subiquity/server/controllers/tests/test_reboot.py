a2545ef7d5d868db47413d1602e1e39bdc0f7e11
Dan Bungert <daniel.bungert@canonical.com>
2021-07-22 17:46:19 -0600

    Skip test on python < 3.7

--- subiquity/server/controllers/tests/test_reboot.py
+++ subiquity/server/controllers/tests/test_reboot.py
@@ -14,6 +14,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime
+import sys
+
 from subiquitycore.tests import SubiTestCase
 from subiquitycore.tests.mocks import make_app
 from subiquitycore.tests.util import run_coro_timeout
@@ -22,42 +24,44 @@ from subiquity.common.types import ApplicationState
 from subiquity.server.controllers.reboot import RebootController
 
 
-class TestRebootController(SubiTestCase):
+# This feature is used with minimum python 3.8 / ubuntu-desktop-installer
+if sys.version >= '3.7.0':
+    class TestRebootController(SubiTestCase):
 
-    # verify that POST returns before the reboot occurrs
-    def test_reboot_event_order(self):
-        async def passer():
-            pass
+        # verify that POST returns before the reboot occurrs
+        def test_reboot_event_order(self):
+            async def passer():
+                pass
 
-        def now():
-            nonlocal exited
-            exited = datetime.now()
+            def now():
+                nonlocal exited
+                exited = datetime.now()
 
-        async def poster():
-            # There is a subtle timing difference between when
-            # run_coro_timeout exits, and when the POST exits, which is
-            # enough to skew results.
-            nonlocal controller
-            nonlocal returned
-            rv = await controller.POST()
-            returned = datetime.now()
-            return rv
+            async def poster():
+                # There is a subtle timing difference between when
+                # run_coro_timeout exits, and when the POST exits, which is
+                # enough to skew results.
+                nonlocal controller
+                nonlocal returned
+                rv = await controller.POST()
+                returned = datetime.now()
+                return rv
 
-        for i in range(100):
-            app = make_app()
-            app.state = ApplicationState.DONE
-            app.controllers.Install.install_task = passer()
-            app.controllers.Late.run_event.wait = passer
-            app.interactive = True
-            app.exit.side_effect = now
+            for i in range(100):
+                app = make_app()
+                app.state = ApplicationState.DONE
+                app.controllers.Install.install_task = passer()
+                app.controllers.Late.run_event.wait = passer
+                app.interactive = True
+                app.exit.side_effect = now
 
-            exited = None
-            returned = None
-            controller = RebootController(app)
-            controller.start()
-            app.exit.assert_not_called()
-            rv = run_coro_timeout(poster(), 1)
-            self.assertTrue(rv)
-            app.exit.assert_called()
-            self.assertTrue(returned < exited,
-                            f'returned {returned} exited {exited}')
+                exited = None
+                returned = None
+                controller = RebootController(app)
+                controller.start()
+                app.exit.assert_not_called()
+                rv = run_coro_timeout(poster(), 1)
+                self.assertTrue(rv)
+                app.exit.assert_called()
+                self.assertTrue(returned < exited,
+                                f'returned {returned} exited {exited}')
