36fe76a96352d7603dba5739496a98c910e4af6f
Chad Smith <chad.smith@canonical.com>
2022-06-15 19:43:24 -0600

    tests: use mock from stdlib unittest to avoid additional test dependency

--- subiquity/tests/test_timezonecontroller.py
+++ subiquity/tests/test_timezonecontroller.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import mock
+from unittest import mock
 
 from subiquity.common.types import TimeZoneInfo
 from subiquity.models.timezone import TimeZoneModel
