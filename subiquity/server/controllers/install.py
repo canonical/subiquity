6252425fcddad74247cef3373d2a87e4edca3c2b
Michael Hudson-Doyle <michael.hudson@canonical.com>
2022-11-25 13:46:52 +1300

    fix description of fstabl step

--- subiquity/server/controllers/install.py
+++ subiquity/server/controllers/install.py
@@ -314,7 +314,7 @@ class InstallController(SubiquityController):
         for step in steps:
             await step(context=context)
 
-    @with_context("creating fstab")
+    @with_context(description="creating fstab")
     async def create_core_boot_classic_fstab(self, *, context):
         with open(self.tpath('etc/fstab'), 'w') as fp:
             fp.write("/run/mnt/ubuntu-boot/EFI/ubuntu /boot/grub none bind\n")
