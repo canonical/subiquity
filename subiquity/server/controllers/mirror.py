5bf552f28b90c05d8d265703d6779c0e077d1ace
Michael Hudson-Doyle <michael.hudson@canonical.com>
2023-03-22 08:55:45 +1300

    create curtin source handlers in a more on-demand fashion
    When we start supporting source variations we are going to need to set
    these up and tear them down a lot more often, trying to keep just one as
    app.controllers.Source._handler is going to get difficult. I actually
    think this is cleaner overall (at least until we get to downloading
    installation sources from the internet...).

--- subiquity/server/controllers/mirror.py
+++ subiquity/server/controllers/mirror.py
@@ -280,12 +280,11 @@ class MirrorController(SubiquityController):
             self.model.set_country(self.app.geoip.countrycode)
         self.cc_event.set()
 
-    def on_source(self):
-        # FIXME disabled until we can sort out umount
-        # if self.apt_configurer is not None:
-        #     await self.apt_configurer.cleanup()
+    async def on_source(self):
+        if self.apt_configurer is not None:
+            await self.apt_configurer.cleanup()
         self.apt_configurer = get_apt_configurer(
-            self.app, self.app.controllers.Source.source_path)
+            self.app, self.app.controllers.Source.get_handler())
         self._apply_apt_config_task.start_sync()
         self.source_configured_event.set()
 
