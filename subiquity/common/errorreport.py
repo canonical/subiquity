232f2f9d8cede6b4c92abd9085b2e41f2218ba17
Dan Bungert <daniel.bungert@canonical.com>
2023-04-12 15:50:22 -0600

    errorreport: add tags capability
    Future problem_report has a nice add_tags method, simulate it here.
    Should be able to drop this with a core24 move.

--- subiquity/common/errorreport.py
+++ subiquity/common/errorreport.py
@@ -22,6 +22,7 @@ import re
 import sys
 import time
 import traceback
+from typing import Iterable, Set
 
 import apport
 import apport.crashdb
@@ -328,6 +329,20 @@ class ErrorReport(metaclass=urwid.MetaSignals):
             seen=self.seen,
             oops_id=self.oops_id,
             )
+
+    # with core24 these tag methods can be dropped for equivalent methods
+    # that will be on the report object
+    def get_tags(self) -> Set[str]:
+        """Return the set of tags."""
+        if "Tags" not in self.pr:
+            return set()
+        return set(self.pr["Tags"].split(" "))
+
+    def add_tags(self, tags: Iterable[str]) -> None:
+        """Add tags to the report. Duplicates are dropped."""
+        current_tags = self.get_tags()
+        new_tags = current_tags.union(tags)
+        self.pr["Tags"] = " ".join(sorted(new_tags))
 
 
 class ErrorReporter(object):
