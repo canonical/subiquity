c1d91d6b991720c3d450ffc5317b943b0620c22d
Dan Bungert <daniel.bungert@canonical.com>
2023-07-24 17:43:21 -0600

    cloudinit: log access to combined-cloud-config
    Co-authored-by: Chad Smith <chad.smith@canonical.com>

--- subiquity/cloudinit.py
+++ subiquity/cloudinit.py
@@ -1,12 +1,20 @@
 """Shared cloudinit utility functions"""
 
 import json
+import logging
+
+log = logging.getLogger("subiquity.cloudinit")
 
 
 def get_host_combined_cloud_config() -> dict:
     """Return the host system /run/cloud-init/combined-cloud-config.json"""
     try:
         with open("/run/cloud-init/combined-cloud-config.json") as fp:
-            return json.load(fp)
+            config = json.load(fp)
+            log.debug(
+                "Loaded cloud config from /run/cloud-init/combined-cloud-config.json"
+            )
+            return config
     except (IOError, OSError, AttributeError, json.decoder.JSONDecodeError):
+        log.debug("Failed to load combined-cloud-config")
         return {}
