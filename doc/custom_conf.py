215e50fa5a85f79dfe2c08f60a95c3f06973f9fd
Robert Krátký <rkratky@gmail.com>
2023-10-17 15:58:32 +0200

    Fix docs path for theme to function.

--- doc/custom_conf.py
+++ doc/custom_conf.py
@@ -72,7 +72,7 @@ html_context = {
 
     # Change to the folder that contains the documentation
     # (usually "/" or "/docs/")
-    'github_folder': '/doc',
+    'github_folder': '/doc/',
 
     # Change to an empty value if your GitHub repo doesn't have issues enabled.
     # This will disable the feedback button and the issue link in the footer.
