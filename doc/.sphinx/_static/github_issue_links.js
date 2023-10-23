4bad05149402c0feaeb49f1a824e0b26a6c6ab2b
Robert Krátký <rkratky@gmail.com>
2023-10-23 16:28:43 +0200

    Update and fix project navigation
    * Header titles & links
    * Footer links
    * Feedback button

--- doc/.sphinx/_static/github_issue_links.js
+++ doc/.sphinx/_static/github_issue_links.js
@@ -11,18 +11,7 @@ window.onload = function() {
     link.classList.add("muted-link");
     link.classList.add("github-issue-link");
     link.text = "Give feedback";
-    link.href = (
-        github_url
-        + "/issues/new?"
-        + "title=docs%3A+TYPE+YOUR+QUESTION+HERE"
-        + "&body=*Please describe the question or issue you're facing with "
-        + `"${document.title}"`
-        + ".*"
-        + "%0A%0A%0A%0A%0A"
-        + "---"
-        + "%0A"
-        + `*Reported+from%3A+${location.href}*`
-    );
+    link.href = ("https://bugs.launchpad.net/subiquity/+filebug");
     link.target = "_blank";
 
     const div = document.createElement("div");
