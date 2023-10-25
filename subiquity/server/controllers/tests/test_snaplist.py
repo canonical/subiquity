0a1f687fb85a3c77f7bee09affda9fc7091bf9cf
Chris Peterson <chris.peterson@canonical.com>
2023-10-25 15:34:23 -0700

    tests: add shared mocks and remove bad import
    This commit re-adds some of the shared mock fields for testing
    and removes a bad import from test_snaplist. These are changes
    that shouldn't have been part of the previously reverted patch:
    0a70a969d42a29d2585d2b1433017217338b762f
    
    (cherry picked from commit 6c27d656f2821a583b231eddc05b34a2db54a0e0)

--- subiquity/server/controllers/tests/test_snaplist.py
+++ subiquity/server/controllers/tests/test_snaplist.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
-from unittest.mock import AsyncMock, Mock
+from unittest.mock import AsyncMock
 
 import requests
 
