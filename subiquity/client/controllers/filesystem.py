88b6dcb6c69778dd502492f6ffdc26bb71746dd8
Olivier Gayot <olivier.gayot@canonical.com>
2024-01-19 17:02:13 +0100

    filesystem: add support for reformatting in run-answers
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- subiquity/client/controllers/filesystem.py
+++ subiquity/client/controllers/filesystem.py
@@ -193,7 +193,10 @@ class FilesystemController(SubiquityTuiController, FilesystemManipulator):
         return raidlevels_by_value[level]
 
     async def _answers_action(self, action):
-        from subiquity.ui.views.filesystem.delete import ConfirmDeleteStretchy
+        from subiquity.ui.views.filesystem.delete import (
+            ConfirmDeleteStretchy,
+            ConfirmReformatStretchy,
+        )
         from subiquitycore.ui.stretchy import StretchyOverlay
 
         log.debug("_answers_action %r", action)
@@ -214,7 +217,9 @@ class FilesystemController(SubiquityTuiController, FilesystemManipulator):
             body = self.ui.body._w
             if not isinstance(body, StretchyOverlay):
                 return
-            if isinstance(body.stretchy, ConfirmDeleteStretchy):
+            if isinstance(
+                body.stretchy, (ConfirmDeleteStretchy, ConfirmReformatStretchy)
+            ):
                 if action.get("submit", True):
                     body.stretchy.confirm()
             else:
