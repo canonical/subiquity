b2068f511438b25b0a9ebf72d50bb414a2af820c
Dan Bungert <daniel.bungert@canonical.com>
2024-04-25 17:32:27 -0600

    quick-test: handle ubuntu-desktop-bootstrap
    (cherry picked from commit 129623cc6b0bbf12fb035f91619b92305ff2ae7b)

--- scripts/quick-test-this-branch.sh
+++ scripts/quick-test-this-branch.sh
@@ -17,8 +17,14 @@ tmpdir="$(mktemp -d)"
 cd "${tmpdir}"
 
 PYTHONPATH=$LIVEFS_EDITOR python3 -m livefs_edit $old_iso /dev/null --setup-rootfs \
-          --shell 'cp rootfs//var/lib/snapd/seed/snaps/subiquity_*.snap '$tmpdir'/old.snap'
+          --shell 'cp rootfs/var/lib/snapd/seed/snaps/subiquity_*.snap '$tmpdir'/old.snap || \
+                   cp rootfs/var/lib/snapd/seed/snaps/ubuntu-desktop-bootstrap_*.snap '$tmpdir'/old.snap'
 
-$src/scripts/slimy-update-snap.sh old.snap subiquity_new.snap
+if [ ! -f old.snap ] ; then
+	echo "subiquity-like snap not found"
+	exit 1
+fi
 
-PYTHONPATH=$LIVEFS_EDITOR python3 -m livefs_edit $old_iso $new_iso --inject-snap subiquity_new.snap
+$src/scripts/slimy-update-snap.sh old.snap new.snap
+
+PYTHONPATH=$LIVEFS_EDITOR python3 -m livefs_edit $old_iso $new_iso --inject-snap new.snap
