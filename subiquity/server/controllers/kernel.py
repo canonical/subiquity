fb9d1b8755531d74278fe49d84b02303f414dd23
Michael Hudson-Doyle <michael.hudson@canonical.com>
2024-10-04 12:19:40 +1300

    broadcast BRIDGE_KERNEL_DECIDED earlier if there is no bridge kernel

--- subiquity/server/controllers/kernel.py
+++ subiquity/server/controllers/kernel.py
@@ -105,6 +105,8 @@ class KernelController(NonInteractiveController):
 
     def _confirmed(self):
         fs_model = self.app.base_model.filesystem
+        if not self.app.base_model.source.catalog.kernel.bridge_reasons:
+            self.app.hub.broadcast(InstallerChannels.BRIDGE_KERNEL_DECIDED)
         self._maybe_set_bridge_kernel(BridgeKernelReason.ZFS, fs_model.uses_zfs())
         if not self.app.base_model.source.search_drivers:
             self._maybe_set_bridge_kernel(BridgeKernelReason.NVIDIA, False)
