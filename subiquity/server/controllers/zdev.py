44aaef3039d844c3daecfa4e82b221d39fab1f5d
Olivier Gayot <olivier.gayot@canonical.com>
2025-02-03 18:25:18 +0100

    zdev: add autoinstall for enabling and disabling devices
    This introduces a new "zdevs" autoinstall, which makes it possible to enable or
    disable supported devices on IBM Z.
    
    One can supply a "zdevs" autoinstall section in the following format:
    
    zdevs:
      - id: 0.0.1506
        enabled: true
      - id: 0.0.1507
        enabled: true
      - id: 0.0.1508
        enabled: false
    
    This is a list since the ordering of operations is important.
    
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- subiquity/server/controllers/zdev.py
+++ subiquity/server/controllers/zdev.py
@@ -14,15 +14,18 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import asyncio
+import itertools
 import logging
 import platform
 from collections import OrderedDict
-from typing import List
+from typing import List, Literal, Optional, TypedDict
 
 from subiquity.common.apidef import API
 from subiquity.common.types import ZdevInfo
 from subiquity.common.types.storage import Bootloader
 from subiquity.server.controller import SubiquityController
+from subiquitycore.async_helpers import schedule_task
+from subiquitycore.context import with_context
 from subiquitycore.utils import run_command
 
 log = logging.getLogger("subiquity.server.controllers.zdev")
@@ -592,10 +595,38 @@ id="0.0.f1fe:0.0.f1ff" type="ctc" on="no" exists="yes" pers="no" auto="no" faile
 id="0.0.c0fe" type="generic-ccw" on="no" exists="yes" pers="no" auto="no" failed="yes" names=""'''  # noqa: E501
 
 
+class ZdevAiItem(TypedDict, total=True):
+    id: str
+    enabled: bool
+
+
+ZdevAi = list[ZdevAiItem]
+
+
 class ZdevController(SubiquityController):
     endpoint = API.zdev
 
+    autoinstall_key = "zdevs"
+    autoinstall_schema = {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "id": {"type": "string"},
+                "enabled": {"type": "boolean"},
+            },
+        },
+    }
+    autoinstall_default = []
+
     def __init__(self, app):
+        self.ai_data: ZdevAi = []
+        self.zdev_handling_task: Optional[asyncio.Task] = None
+
+        # Recording of actions that have been performed on the Zdevs. This is
+        # only used to produce an autoinstall config at the end.
+        self.done_actions: list[ZdevAiItem] = []
+
         super().__init__(app)
         if self.opts.dry_run:
             if platform.machine() == "s390x":
@@ -606,19 +637,55 @@ class ZdevController(SubiquityController):
                 zdevinfos = [ZdevInfo.from_row(row) for row in devices]
             self.zdevinfos = OrderedDict([(i.id, i) for i in zdevinfos])
 
+    def load_autoinstall_data(self, data: ZdevAi) -> None:
+        self.ai_data = data
+
+    @with_context()
+    async def apply_autoinstall_config(self, context) -> None:
+        if self.zdev_handling_task is not None:
+            await self.zdev_handling_task
+
+    def start(self) -> None:
+        if self.ai_data:
+            self.zdev_handling_task = schedule_task(self.handle_zdevs())
+
+    def make_autoinstall(self) -> ZdevAi:
+        # Small "optimization" to avoid producing a config that enables or
+        # disables a given device multiple times in a row.
+        return [x for x, _ in itertools.groupby(self.done_actions)]
+
+    async def handle_zdevs(self) -> None:
+        for item in self.ai_data:
+            action = "enable" if item["enabled"] else "disable"
+            await self.chzdev(action, self.zdevinfos[item["id"]])
+
     def interactive(self):
         if self.app.base_model.filesystem.bootloader != Bootloader.NONE:
             return False
         return super().interactive()
 
-    async def chzdev_POST(self, action: str, zdev: ZdevInfo) -> List[ZdevInfo]:
+    async def chzdev(
+        self, action: Literal["enable", "disable"], zdev: ZdevInfo
+    ) -> None:
+        if action == "enable":
+            on = True
+            state = "enabled"
+        elif action == "disable":
+            on = False
+            state = "disabled"
+        else:
+            raise ValueError("action must be 'enable' or 'disable'")
+
+        self.done_actions.append(ZdevAiItem(id=zdev.id, state=state))
+
         if self.opts.dry_run:
-            on = action == "enable"
             self.zdevinfos[zdev.id].on = on
             self.zdevinfos[zdev.id].pers = on
         chzdev_cmd = ["chzdev", "--%s" % action, zdev.id]
         await self.app.command_runner.run(chzdev_cmd)
 
+    async def chzdev_POST(self, action: str, zdev: ZdevInfo) -> List[ZdevInfo]:
+        await self.chzdev(action, zdev)
         return await self.GET()
 
     async def GET(self) -> List[ZdevInfo]:
