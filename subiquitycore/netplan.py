08178d03bc0daa0a74cd36ec950bdfb9241ad092
Olivier Gayot <olivier.gayot@canonical.com>
2025-09-12 12:54:33 +0200

    netplan: if multiple configs match, behave more like netplan
    If multiple sections match a given interface name (i.e., ens3 here),
    netplan (or the backend really) will honor the first matching section
    ordered by section
    name.
    
      ethernets:
        cc:
          match: {name: ens3}
          addresses: [2.2.2.2/24]
        bb:
          match: {name: ens3}
          addresses: [1.1.1.1/24]
        aa:
          match: {name: lo}
          addresses: [127.0.0.1/8]
        ens3:
          addresses: [3.3.3.3/24]
    
    In this scenario, that would be "bb" because it matches the interface
    name and "bb" comes before "ens3" in lexical order.
    
    Previously, Subiquity would honor the first matching section (i.e., cc).
    This would result in `netplan apply` to honor "bb" but subiquity to
    apply "cc" when reaching the network screen.
    
    Fixed by making Subiquity behave more like netplan. We now sort by
    section name when determining the right configuration to apply.
    
    LP: #2122583
    
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- subiquitycore/netplan.py
+++ subiquitycore/netplan.py
@@ -69,13 +69,13 @@ class Config:
 
     def config_for_device(self, link):
         if link.is_virtual:
-            for dev in self.virtual_devices:
+            for dev in sorted(self.virtual_devices, key=lambda x: x.name):
                 if dev.name == link.name:
                     return copy.deepcopy(dev.config)
         else:
             allowed_matches = ("macaddress",)
             match_key = "match"
-            for dev in self.physical_devices:
+            for dev in sorted(self.physical_devices, key=lambda x: x.name):
                 if dev.matches_link(link):
                     config = copy.deepcopy(dev.config)
                     if match_key in config:
@@ -108,6 +108,11 @@ class Config:
 
 class _PhysicalDevice:
     def __init__(self, name, config):
+        # Name of the config section.
+        # Different configs are sorted by section name (using lexical order).
+        # If two config sections match the same interface, whichever comes
+        # first is honored.
+        self.name = name
         match = config.get("match")
         if match is None:
             self.match_name = name
