b432ed4c4ab79db75a88b665b57ac2c6ed4009ab
Olivier Gayot <olivier.gayot@canonical.com>
2025-11-25 14:17:35 +0100

    shutdown: move functions to a different file
    Instead of keeping everything in the shutdown controller, we now implement the
    actual shutdown related operations in a dedicated module.
    
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>
    (cherry picked from commit c79ea2f290897ee98656387cba753e2a6c424428)

---
+++ subiquity/server/tests/test_shutdown.py
@@ -0,0 +1,59 @@
+# Copyright 2025 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+import unittest
+import unittest.mock
+
+from subiquity.server import shutdown
+
+
+@unittest.mock.patch("subiquity.server.shutdown.arun_command")
+class TestInitiateShutdown(unittest.IsolatedAsyncioTestCase):
+    async def test_reboot_success(self, arun_command):
+        await shutdown.initiate_reboot()
+        arun_command.assert_called_once_with(
+            ["systemctl", "reboot", "--ignore-inhibitors"]
+        )
+
+    async def test_reboot_failure(self, arun_command):
+        arun_command.side_effect = subprocess.CalledProcessError(
+            cmd=["systemctl", "..."], returncode=1, stderr="Permission Denied"
+        )
+
+        with self.assertRaises(subprocess.CalledProcessError):
+            await shutdown.initiate_reboot()
+
+        arun_command.assert_called_once_with(
+            ["systemctl", "reboot", "--ignore-inhibitors"]
+        )
+
+    async def test_poweroff_success(self, arun_command):
+        await shutdown.initiate_poweroff()
+        arun_command.assert_called_once_with(
+            ["systemctl", "poweroff", "--ignore-inhibitors"]
+        )
+
+    async def test_poweroff_failure(self, arun_command):
+        arun_command.side_effect = subprocess.CalledProcessError(
+            cmd=["systemctl", "..."], returncode=1, stderr="Permission Denied"
+        )
+
+        with self.assertRaises(subprocess.CalledProcessError):
+            await shutdown.initiate_poweroff()
+
+        arun_command.assert_called_once_with(
+            ["systemctl", "poweroff", "--ignore-inhibitors"]
+        )
