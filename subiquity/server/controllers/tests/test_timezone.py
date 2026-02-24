4ec4c08dd0edc509eef619a5b3d0df420319fd0a
Olivier Gayot <olivier.gayot@canonical.com>
2026-02-24 14:23:44 +0100

    timezone: raise an autoinstall error if autoinstall contains invalid timezone
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>
    (cherry picked from commit c3551f084e2211cc51bf9725b63943ca45786bb4)
    
    Modified to:
     * adapt to UnknownTimezoneError not being defined in ubuntu/noble
     * timezone unit tests being scattered across two files in ubuntu/noble

--- subiquity/server/controllers/tests/test_timezone.py
+++ subiquity/server/controllers/tests/test_timezone.py
@@ -13,14 +13,44 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from unittest import mock
+
 import jsonschema
 from jsonschema.validators import validator_for
 
+from subiquity.models.timezone import TimeZoneModel
+from subiquity.server.autoinstall import AutoinstallError
 from subiquity.server.controllers.timezone import TimeZoneController
+from subiquity.tests.test_timezonecontroller import MockGeoIP, tz_denver
 from subiquitycore.tests import SubiTestCase
+from subiquitycore.tests.mocks import make_app
 
 
 class TestTimeZoneController(SubiTestCase):
+    def setUp(self):
+        self.tzc_init()
+
+    def tzc_init(self):
+        self.tzc = TimeZoneController(make_app())
+        self.tzc.model = TimeZoneModel()
+        self.tzc.app.geoip = MockGeoIP()
+        self.tzc.app.geoip.text = tz_denver
+
+    def test_load_autoinstall_data(self):
+        data = "Europe/Kyiv"
+        with mock.patch.object(self.tzc, "deserialize"):
+            self.tzc.load_autoinstall_data(data)
+
+    def test_load_autoinstall_data__invalid_timezone(self):
+        data = "Europe/Kiev"
+        with mock.patch.object(
+            self.tzc,
+            "deserialize",
+            side_effect=ValueError('Unrecognized time zone request "Europe/Kiev"'),
+        ):
+            with self.assertRaises(AutoinstallError):
+                self.tzc.load_autoinstall_data(data)
+
     def test_valid_schema(self):
         """Test that the expected autoinstall JSON schema is valid"""
 
