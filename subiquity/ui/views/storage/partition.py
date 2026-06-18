444a55942313e891f42ebed07fb7e1696326d4c9
Olivier Gayot <olivier.gayot@canonical.com>
2026-06-18 22:52:55 +0200

    storage: update module docstring of partition.py
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- subiquity/ui/views/storage/partition.py
+++ subiquity/ui/views/storage/partition.py
@@ -13,10 +13,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Filesystem
+"""Partition
 
-Provides storage device selection and additional storage
-configuration.
+Provides partition creation utilities.
 
 """
 import itertools
